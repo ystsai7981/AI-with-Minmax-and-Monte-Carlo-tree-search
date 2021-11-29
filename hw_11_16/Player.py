c26a1b91d32343446b42b63095e50cca4532151b
ystsai <ystsai7981@gmail.com>
2021-11-29 14:05:04 +0800

    unfinished

--- hw_11_16/Player.py
+++ hw_11_16/Player.py
@@ -16,18 +16,24 @@ class Player:
             (1, 0),
             (1, 1)
         )
+        self.cornor = (
+            (0, 0),
+            (0, 7),
+            (7, 0),
+            (7, 7)
+        )
+        self.start_level = 0
+        self.search_levels = 3
         self.player_no = 1
-        self.valid_loc = []
-        self.valid_moves = {1:[], -1:[]}
-        self.score = {}
-        self.weight = [[500,  5, 100, 50, 50, 100,  5, 500],
-                       [  5,  1,  10, 10, 10,  10,  1,   5],
-                       [100, 10,  30, 20, 20,  30, 10, 100],
-                       [ 50, 10,  20, 10, 10,  20, 10,  50],
-                       [ 50, 10,  20, 10, 10,  20, 10,  50],
-                       [100, 10,  30, 20, 20,  30, 10, 100],
-                       [  5,  1,  10, 10, 10,  10,  1,   5],
-                       [500,  5, 100, 50, 50, 100,  5, 500]]
+        # self.weight = [[500,  5, 100, 50, 50, 100,  5, 500],
+        #                [  5,  1,  10, 10, 10,  10,  1,   5],
+        #                [100, 10,  30, 20, 20,  30, 10, 100],
+        #                [ 50, 10,  20, 10, 10,  20, 10,  50],
+        #                [ 50, 10,  20, 10, 10,  20, 10,  50],
+        #                [100, 10,  30, 20, 20,  30, 10, 100],
+        #                [  5,  1,  10, 10, 10,  10,  1,   5],
+        #                [500,  5, 100, 50, 50, 100,  5, 500]]
+        self.c = np.sqrt(2*np.log(2)/np.log(np.e))
 
     def move(self, board_inf):
         """
@@ -40,40 +46,37 @@ class Player:
         return:
             your moves: 你要下哪裡，它會是一個一維的 list ex:[1,2]
         """
-        if board_inf[3] < 40:
-            # return choice(board_inf[0])
-            max_total = 10
-        # elif board_inf[3] < 40:
-            # max_total = 20
-        else:
-            max_total = 20
-        # max_total = 100
         self.player_no = board_inf[2]
-        self.valid_loc.clear()
-        self.valid_moves.clear()
-        self.score.clear()
-        # print(board_inf[1])
+        valid_loc = []
+        valid_moves = {1:[], -1:[]}
         for row, col in np.argwhere(board_inf[1] != 0):
-            self.add_valid_loc(row, col, board_inf[1], self.valid_loc)
-        self.update_valid_moves(board_inf[1], self.valid_loc, self.valid_moves)
-        unique_valid_moves = np.unique(np.array(self.valid_moves[self.player_no])[:, :2], axis=0)
-        for idx, move in enumerate(unique_valid_moves):
-            win = 0
-            for i in range(max_total):
-                win += self.simulate(board_inf[1], self.player_no, move, self.valid_loc, self.valid_moves)
-            self.score[idx] = win/max_total
-        # self.find_best()
-            
-        return unique_valid_moves[max(self.score, key=self.score.get)]
+            self.add_valid_loc(row, col, board_inf[1], valid_loc)
+        self.update_valid_moves(board_inf[1], valid_loc, valid_moves)
+        
+        # if board_inf[3] < 40:
+            # return choice(board_inf[0])
+            # max_total = 50
+        # else:
+            # max_total = 100
+
+        # return self.MCS(board_inf[1], self.player_no, valid_loc, valid_moves, max_total)
+        # return self.MCS_UCB(board_inf[1], self.player_no, valid_loc, valid_moves, max_total)
+        return self.find_max(board_inf[1], self.player_no, valid_loc, valid_moves, self.start_level)
+        # return unique_valid_moves[max(score, key=score.get)]
     
     def isInside(self, row, col) -> bool:
         return True if 0 <= row < 8 and 0 <= col < 8 else False
         
     def check_who_wins(self, board_status):
         players, counts = np.unique(board_status, return_counts=True)
-        # print("players counts:", players, counts)
-        # print("winner:", players[np.argmax(counts)])
-        return players[np.argmax(counts)]
+        result = {}
+        for p, c in zip(players, counts):
+            result[p] = c
+        if result[1] == result[-1]:
+            return 0
+        else:
+            return max(result, key=result.get)
+            
     
     def add_valid_loc(self, row, col, board_state, valid_loc):
         if [row, col] in valid_loc:
@@ -136,10 +139,75 @@ class Player:
         else:
             return 0
                 
-    def find_best():
-        pass
+    def MCS(self, board_state, current_player, valid_loc, valid_moves, max_total):
+        score = {}
+        unique_valid_moves = np.unique(np.array(valid_moves[current_player])[:, :2], axis=0)
+        for idx, move in enumerate(unique_valid_moves):
+            win = 0
+            for i in range(max_total):
+                win += self.simulate(board_state, current_player, move, valid_loc, valid_moves)
+            score[idx] = win/max_total
+        return unique_valid_moves[max(score, key=score.get)]
+    
+    def MCS_UCB(self, board_state, current_player, valid_loc, valid_moves, max_total):
+        score = {}
+        Wi = {}
+        Ni = {}
+        UCB = {}        
+        unique_valid_moves = np.unique(np.array(valid_moves[current_player])[:, :2], axis=0)
+        for idx, move in enumerate(unique_valid_moves):
+            Wi[idx] = self.simulate(board_state, current_player, move, valid_loc, valid_moves)
+            Ni[idx] = 1
+        for N in range(1, max_total+1):
+            UCB.clear()
+            for idx in range(len(unique_valid_moves)):
+                UCB[idx] = Wi[idx]/Ni[idx] + self.c*np.sqrt(np.log(N)/Ni[idx])
+            move_id = max(UCB, key=UCB.get)
+            Wi[move_id] += self.simulate(board_state, current_player, unique_valid_moves[move_id], valid_loc, valid_moves)
+            Ni[move_id] += 1
+        for idx in range(len(unique_valid_moves)):
+            score[idx] = Wi[idx]/Ni[idx]
+        return unique_valid_moves[max(score, key=score.get)]
+    
+    def get_value(self, board_state, current_player, valid_moves):
+        unique_valid_moves = np.unique(np.array(valid_moves[current_player])[:, :2], axis=0)
+        mobility = len(unique_valid_moves)
+        disc_count = 0.01*len(np.argwhere(board_state == current_player))
+        cornor_bonus = 0
+        for cornor in self.cornor:
+            if (unique_valid_moves == cornor).all(axis=1).any():
+                cornor_bonus += 10
+        return mobility + disc_count + cornor_bonus
+    
+    def get_endgame_value(self, board_state, current_player):
+        winner = self.check_who_wins(board_state)
+        if winner == current_player:
+            return 100
+        elif winner == 0:
+            return 20
+        else:
+            return 0
+    
+    def find_max(self, board_state, current_player, valid_loc, valid_moves, level):
+        if not (valid_moves[current_player] or valid_moves[-current_player]):
+            return self.get_endgame_value(board_state, current_player)
+        if level >= self.search_levels:
+            return self.get_value(board_state, current_player, valid_moves)
+        else:
+            if not valid_moves[current_player]:
+                return self.find_max(board_state, -current_player, valid_loc, valid_moves, level+1)
+            else:
+                value = {}
+                unique_valid_moves = np.unique(np.array(valid_moves[current_player])[:, :2], axis=0)
+                for idx, move in enumerate(unique_valid_moves):
+                    board_cp = board_state.copy()
+                    valid_loc_cp = valid_loc.copy()
+                    valid_moves_cp = valid_moves.copy()
+                    self.action(board_cp, current_player, move, valid_loc_cp, valid_moves_cp)
+                    value[idx] = self.find_max(board_cp, -current_player, valid_loc_cp, valid_moves_cp, level+1)
+                if not level:
+                    return unique_valid_moves[max(value, key=value.get)]
+                else:
+                    return max(value, key=value.get)
         
     
-
-        
-        
